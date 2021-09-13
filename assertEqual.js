c47deb23b2b9973e6de7a5a66bad39b3a619ae1d
Joseph Shin <seoung.shin@ontariotechu.net>
2021-09-13 13:33:35 -0400

    LHL w1d1 assert exercise

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.assert(actual === expected, "🛑🛑🛑 Assertion failed " + actual + " !== " + expected); 
+}
+  else {
+console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+      }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
