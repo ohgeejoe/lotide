fa42d3c431d150e2d847d7dd82b86cc02d138b6b
Joseph Shin <seoung.shin@ontariotechu.net>
2021-09-15 20:50:24 -0400

    LHL intial commit

---
+++ findKeyByValue.js
@@ -0,0 +1,31 @@
+//the fucntion findKeyByValue takes in an object and a value. It scans the object and returns the first first which contains the given value.
+
+//pasting assertEqual
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.assert(actual === expected, `🛑🛑🛑 Assertion failed ${actual} !== ${expected}`); 
+  }
+  else {
+    console.log(`✅✅✅ Assertion Passed:  + ${actual} === ${expected}`);
+  }
+};
+
+const findKeyByValue = function(inputObject,inputKey) {
+  const objectKeys = Object.keys(inputObject); //object.keys returns an array of the objects key names.
+  for (let key of objectKeys) { //the for of loop creates a loop iterating thorugh the object. key is the element, ObjectKeys is the object
+    if (inputObject[key] === inputKey) {
+      return key;
+    }
+
+  }
+}
+
+// Test code
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
