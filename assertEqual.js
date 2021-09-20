0268a72f2532863234959f8a7b4ef01e4d44faf8
Joseph Shin <seoung.shin@ontariotechu.net>
2021-09-20 10:01:05 -0400

    added module export

--- assertEqual.js
+++ assertEqual.js
@@ -1,13 +1,10 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual !== expected) {
-    console.assert(actual === expected, `🛑🛑🛑 Assertion failed ${actual} !== ${expected}`); 
-}
-  else {
-console.log(`✅✅✅ Assertion Passed:  + ${actual} === ${expected}`);
-      }
+    console.assert(actual === expected, `🛑🛑🛑 Assertion failed ${actual} !== ${expected}`);
+  } else {
+    console.log(`✅✅✅ Assertion Passed:  + ${actual} === ${expected}`);
+  }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+module.exports = assertEqual;
